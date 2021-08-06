0448fbe0d788ac1cfb1b65419647eecc90628df4
Samanta Fluture <samantafluture@gmail.com>
2021-08-05 22:08:16 -0300

    studying mongoddb and mongoose

--- 5 - MongoDB/nodeEventos/app.js
+++ 5 - MongoDB/nodeEventos/app.js
@@ -27,24 +27,14 @@ mongoose.connection.on("disconnected", function () {
 
 /* Configurações */
 
-// app -> objeto que será nossa instância do express importado acima
-// essa variável executará o que significa o próprio módulo
 app = express();
 
-// o primeiro valor de "views" setado acima é uma constante do express
-// significa que ao configurarmos, falamos para a app que nossas views não podem
-// ficar em qualquer local, mas sim em um específico -> /.views
 app.set("views", __dirname + "/views");
 app.set("models", __dirname + "/models");
 app.set("controllers", __dirname + "/controllers");
-
-// ejs -> mecanismo de execução da views (extensão do arquivo)
-// view engine -> mecanismo que a app usará para executar as telas
 app.set("view engine", "ejs");
 
-// config de caminho de pasta raiz para recursos estáticos
 app.use(express.static(__dirname + "/public"));
-
 app.use(cookieParser("nodeEventos"));
 app.use(expressSession());
 app.use(bodyParser.json());
@@ -53,13 +43,10 @@ app.use("/favicon.ico", express.static("public/images/favicon.ico"));
 
 /* Carregar o app */
 
-// ordem de carregamento para carregar o app
 load("models").then("controllers").then("routes").into(app);
 
 /* Rodar o servidor */
 
-// listen -> para rodar servidor
-// express encapsula módulo de http e usa direto na chamada
 app.listen(3001, function () {
   console.log("Aplicação rodando!");
 });

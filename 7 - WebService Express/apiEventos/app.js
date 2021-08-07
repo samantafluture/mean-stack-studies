bc590fad521b21f9e6774915d1985564c3a9c214
Samanta Fluture <samantafluture@gmail.com>
2021-08-06 23:00:01 -0300

    json ready with mongoose

--- 7 - WebService Express/apiEventos/app.js
+++ 7 - WebService Express/apiEventos/app.js
@@ -1,41 +1,52 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/* Importações */
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var express = require("express");
+var load = require("express-load");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-var app = express();
+/* Configurações */
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+global.db = mongoose.connect("mongodb://localhost:27017/turma30", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+load("models").into(app);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+var Evento = app.models.eventosm;
+
+/* Rotas */
+
+app.get("/", function (request, response) {
+  response.send("WebService no ar :)");
 });
 
-module.exports = app;
+app.get("/eventos", function (request, response) {
+  Evento.find(function (erro, retorno) {
+    if (erro) {
+      response.json(erro);
+    } else {
+      response.json(retorno);
+    }
+  });
+});
+
+app.get("/evento/:id", function (request, response) {});
+
+app.post("/evento", function (request, response) {});
+
+app.put("/evento/:id", function (request, response) {});
+
+app.delete("/evento/:id", function (request, response) {});
+
+/* Rodar servidor */
+
+app.listen(3200, function () {
+  console.log("API rodando :)");
+});

5f3d1d7c614c567e57e2677b95f4163a606a5fb9
Samanta Fluture <samantafluture@gmail.com>
2021-08-09 23:54:43 -0300

    create basic angularjs crud app with my eventos api

--- 09-angularjs-conceitos/angularJS/app.js
+++ 09-angularjs-conceitos/angularJS/app.js
@@ -6,6 +6,7 @@ app = express();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
